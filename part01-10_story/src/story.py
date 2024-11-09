207344b19e98daa4beb8044b464f89159115d005
Dharam Shah <dharam_shah@hotmail.co.uk>
2024-11-09 13:39:07 +0000

    Complete exercises for Part 1 Section 1 & 2: Getting Started + Information from the User

--- part01-10_story/src/story.py
+++ part01-10_story/src/story.py
@@ -2,8 +2,6 @@
 name = input("Please type in a name: ")
 year = input("Please type in a year: ")
 
-print(name + " is a valiant knight, born in the year")
-print(year + ". One morning " + name + " woke up to an")
-print("awful racket: a dragon was approaching the")
-print("village. Only " + name + " could save the village's")
-print("residents.")
+print(name + " is a valiant knight, born in the year " + year + ". One morning " + name + " woke up to an")
+print("awful racket: a dragon was approaching the village. Only " + name + " could save the")
+print("village's residents.")
