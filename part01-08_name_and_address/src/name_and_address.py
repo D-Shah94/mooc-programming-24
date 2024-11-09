72fc0a2b7899680519970005088368f2ff4d7a81
Dharam Shah <dharam_shah@hotmail.co.uk>
2024-11-09 12:43:25 +0000

    Initial commit

---
+++ part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,10 @@
+# Write your solution here
+name = input("Given name: ")
+family_name = input("Family name: ")
+address = input("Street address: ")
+city_and_postcode = input("City and postal code: ")
+
+print(name + " " + family_name)
+print(address)
+print(city_and_postcode)
+
---
+++ part01-10_story/src/story.py
@@ -0,0 +1,9 @@
+# Write your solution here
+name = input("Please type in a name: ")
+year = input("Please type in a year: ")
+
+print(name + " is a valiant knight, born in the year")
+print(year + ". One morning " + name + " woke up to an")
+print("awful racket: a dragon was approaching the")
+print("village. Only " + name + " could save the village's")
+print("residents.")
