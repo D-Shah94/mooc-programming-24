70ccb3981c6f7a7a18257c2ef9f25d614db7c2e8
Dharam Shah <dharam_shah@hotmail.co.uk>
2024-11-15 16:18:51 +0000

    Complete exercises for Part 2 Section 1 & 2: Programming Terminology + More Conditionals

--- part02-07_alphabetically_last/src/alphabetically_last.py
+++ part02-07_alphabetically_last/src/alphabetically_last.py
@@ -1,1 +1,11 @@
 # Write your solution here
+
+first_word = str(input("Please type in the 1st word: "))
+second_word = str(input("Please type in the 2nd word: "))
+
+if first_word < second_word:
+    print(f"{second_word} comes alphabetically last.")
+elif second_word < first_word:
+    print(f"{first_word} comes alphabetically last.")
+else:
+    print("You gave the same word twice.")
